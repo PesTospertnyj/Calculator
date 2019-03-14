bb95c92ae1831f8634eab7efbc79c4c108021c9d
PesTospertnyj <gonartur@ukr.net>
2019-03-14 18:57:43 +0200

    2

--- MateAcademyTestExersise/src/calculator/Calculator.java
+++ MateAcademyTestExersise/src/calculator/Calculator.java
@@ -4,73 +4,76 @@ import java.util.Scanner;
 
 public class Calculator {
 
-	public static final int I = 1;
-	public static final int II = 2;
-	public static final int III = 3;
-	public static final int IV = 4;
-	public static final int V = 5;
-	public static final int VI = 6;
-	public static final int VII = 7;
-	public static final int VIII = 8;
-	public static final int IX = 9;
-	public static final int X = 10;
+    static Scanner scanner = new Scanner(System.in);
 
-	static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		int num1 = getInt();
-		int num2 = getInt();
-		char operation = getOperation();
-		int result = calc(num1, num2, operation);
-		System.out.println("Результат операции: " + result);
-	}
+        RomanNums rn1 = RomanNums.I;
+        RomanNums rn2 = RomanNums.II;
+        RomanNums rn3 = RomanNums.III;
+        RomanNums rn4 = RomanNums.IV;
+        RomanNums rn5 = RomanNums.V;
+        RomanNums rn6 = RomanNums.VI;
+        RomanNums rn7 = RomanNums.VII;
+        RomanNums rn8 = RomanNums.VIII;
+        RomanNums rn9 = RomanNums.IX;
+        RomanNums rn10 = RomanNums.X;
 
-	public static int getInt() {
-		System.out.println("Введите число:");
-		int num;
-		if (scanner.hasNextInt()) {
-			num = scanner.nextInt();
-		} else {
-			System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
-			scanner.next();
-			num = getInt();
-		}
-		return num;
-	}
 
-	public static char getOperation() {
-		System.out.println("Введите операцию:");
-		char operation;
-		if (scanner.hasNext()) {
-			operation = scanner.next().charAt(0);
-		} else {
-			System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
-			scanner.next();
-			operation = getOperation();
-		}
-		return operation;
-	}
+        int num1 = getInt();
+        int num2 = getInt();
+        char operation = getOperation();
+        int result = calc(num1, num2, operation);
+        System.out.println("Результат операции: " + result);
+    }
 
-	public static int calc(int num1, int num2, char operation) {
-		int result;
-		switch (operation) {
-		case '+':
-			result = num1 + num2;
-			break;
-		case '-':
-			result = num1 - num2;
-			break;
-		case '*':
-			result = num1 * num2;
-			break;
-		case '/':
-			result = num1 / num2;
-			break;
-		default:
-			System.out.println("Операция не распознана. Повторите ввод.");
-			result = calc(num1, num2, getOperation());
-		}
-		return result;
-	}
+    public static int getInt() {
+        System.out.println("Введите число:");
+        int num;
+        if (scanner.hasNextInt() ) {
+            num = scanner.nextInt();
+            //num = scanner.next();
+        } else {
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            scanner.next();
+            num = getInt();
+        }
+        return num;
+    }
+
+    public static char getOperation() {
+        System.out.println("Введите операцию:");
+        char operation;
+        if (scanner.hasNext()) {
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+    public static int calc(int num1, int num2, char operation) {
+        int result;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = calc(num1, num2, getOperation());
+        }
+        return result;
+    }
 
 }
---
+++ MateAcademyTestExersise/src/calculator/RomanNums.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public enum RomanNums {
+    I(1), II(2), III(3), IV(4), V(5), VI(6), VII(7), VIII(8), IX(9), X(10);
+
+    RomanNums(int i) {
+    }
+}
